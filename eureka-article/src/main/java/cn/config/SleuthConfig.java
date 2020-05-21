fcbed914cc76035b6ff2646ead3df048779da894
梁胜楠 <1443895720@qq.com>
2020-05-21 10:07:09 +0800

    Sleuth 提交

---
+++ eureka-article/src/main/java/cn/config/SleuthConfig.java
@@ -0,0 +1,29 @@
+package cn.config;
+
+import brave.http.HttpAdapter;
+import brave.http.HttpSampler;
+import org.springframework.cloud.sleuth.instrument.web.ServerSampler;
+import org.springframework.cloud.sleuth.instrument.web.SkipPatternProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.regex.Pattern;
+
+@Configuration
+public class SleuthConfig {
+    @Bean(name = ServerSampler.NAME)
+    public HttpSampler myHttpSampler(SkipPatternProvider patternProvider) {
+        Pattern pattern = patternProvider.skipPattern();
+        return new HttpSampler(){
+            @Override
+            public <Req> Boolean trySample(HttpAdapter<Req, ?> httpAdapter, Req req) {
+                String url = httpAdapter.url(req);
+                boolean skip = pattern.matcher(url).matches();
+                if(skip) {
+                    return false;
+                }
+                return null;
+            }
+        };
+    }
+}
